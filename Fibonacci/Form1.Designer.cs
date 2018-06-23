021e42e438c3dcbef9346678af8bc74a47607b53
alpaca <36502013+Alpaca-Tech@users.noreply.github.com>
2018-06-23 14:22:42 +0800

    Add files via upload

---
+++ Fibonacci/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Fibonacci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_show = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "費氏數列的前80項";
+            // 
+            // tb_show
+            // 
+            this.tb_show.Font = new System.Drawing.Font("新細明體", 15F);
+            this.tb_show.Location = new System.Drawing.Point(12, 39);
+            this.tb_show.Multiline = true;
+            this.tb_show.Name = "tb_show";
+            this.tb_show.ReadOnly = true;
+            this.tb_show.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_show.Size = new System.Drawing.Size(279, 293);
+            this.tb_show.TabIndex = 3;
+            this.tb_show.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 347);
+            this.Controls.Add(this.tb_show);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Fibonacci";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_show;
+    }
+}
+
---
+++ Fibonacci/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fibonacci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                int nb = 80;
+                long[] my_arr = new long[nb+1];
+                my_arr[0] = 0; my_arr[1] = 1;   //定義起始f(0) 與 f(1)
+
+                for (int i = 2; i < nb+1; i++)
+                {
+                    my_arr[i] = (my_arr[i - 1] + my_arr[i - 2]);
+                }
+                for (int i = 1; i < nb+1; i++)
+                {
+                    tb_show.Text += "f(" + (i).ToString() + ") = " + (my_arr[i]).ToString() + "\r\n";
+                }
+            }
+            catch { my_error(); }
+            
+        }
+
+
+
+        private void my_error(){
+            MessageBox.Show("Error!", "Error!");
+        }
+
+
+    }
+}
